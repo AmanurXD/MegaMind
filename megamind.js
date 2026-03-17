6b1be11637f26e7e88b456e0f208c1db947d571d
Jame's Code <amanurxd@gmail.com>
2026-03-17 12:33:49 -0700

    Update RAW_REQUEST to new user post ID

--- megamind.js
+++ megamind.js
@@ -3,15 +3,15 @@ const https = require('https');
 // ==========================================
 // 🔧 CONFIGURATION (PASTE YOUR RAW REQUEST HERE)
 // ==========================================
-const RAW_REQUEST = `GET /users/posts/bump/50511057 HTTP/2
+const RAW_REQUEST = `GET /users/posts/bump/50707069 HTTP/2
 Host: megapersonals.eu
-Cookie: _ym_uid=1773621904359197761; _ym_d=1773621904; visitorId=59efe8d4-a629-406c-a4a3-839c89aa573f; termsOfUseVersion=2; _ga_7DGFPGNTB9=GS2.1.s1773715162$o4$g0$t1773715162$j60$l0$h0; _ga=GA1.1.1379120135.1773621910; _gid=GA1.2.1473477669.1773621911; publicDomain=megapersonals.eu; mp_screen_client=1920x947; sid=d95e1e491712c0a830f78836e564714b; backURL=https%3A%2F%2Fmegapersonals.eu%2Fusers%2Fposts%2Fselect%2F50511057; _ym_isad=2; JSESSIONID=C2152B79FAC2C99A5C261485729F418C; __cf_bm=cPI677zelA2jWQMCyicgBg__MStRTcNNHOYPSSo1w2E-1773715155.1254668-1.0.1.1-CbqW30xj3QcqqGvB1fQM7Vn_UEsy9zm5N4d3rtyMY2zAwkiLd7UmOgLRUnlosxpd_rs9w3MEI0m5Z6Cm2BgOXhxL_La9RX8RwlwafpRFFCQouJprreOxffo1ndEnVtJO; _gat_gtag_UA_113349993_1=1
+Cookie: _ym_uid=1773621904359197761; _ym_d=1773621904; visitorId=59efe8d4-a629-406c-a4a3-839c89aa573f; termsOfUseVersion=2; _ga_7DGFPGNTB9=GS2.1.s1773775740$o5$g1$t1773775887$j40$l0$h0; _ga=GA1.2.1379120135.1773621910; _gid=GA1.2.1473477669.1773621911; publicDomain=megapersonals.eu; mp_screen_client=1920x947; sid=d95e1e491712c0a830f78836e564714b; backURL=https%3A%2F%2Fmegapersonals.eu%2Fusers%2Fposts%2Fselect%2F50707069; _ym_isad=2; JSESSIONID=848B9D32A18E6E6FB3511EE706283972; __cf_bm=853SUGFt9x3vMWXOf07vJDtFWuswWO48Iw0.KUiUNbY-1773775736.9661338-1.0.1.1-_irSTO9beSXjHWTLS4_MXC1y8WDUGEEdoVrJWSkWBRQPgebXOKejz_K.cFAUctb8YJB_7CF9_6C1oC5KHAe7OhiPtj3vsrCwxqgLBjT3QwJOIxm6TT8LwEiEPAH5vlTI
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:148.0) Gecko/20100101 Firefox/148.0
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
 Accept-Language: en-US,en;q=0.9
 Accept-Encoding: gzip, deflate, br
 Sec-Gpc: 1
-Referer: https://megapersonals.eu/users/posts/select/50511057
+Referer: https://megapersonals.eu/users/posts/select/50707069
 Upgrade-Insecure-Requests: 1
 Sec-Fetch-Dest: document
 Sec-Fetch-Mode: navigate
